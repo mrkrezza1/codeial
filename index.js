160a7df92d7618ee569b4d901a2000b336774ae0
mrkrezza <sajit1020@gmail.com>
2023-04-18 12:47:39 +0530

    first commit in git:

---
+++ index.js
@@ -0,0 +1,14 @@
+// first step is to make structural directory
+// second step here for creating the directory we have given a command that is (mkdir views,models,controllers,routers,config)
+// third step is to install express by givin command in the terminal that is npm install express
+// fourth step is to set the npm start  through json file automatically for that you have to pass the command in the json file as like "start":"nodemon index.js"
+// fifth step is to set the git repository by adding ( in these u got eligibility to ignore the loads of file just by writing some commands in that file )
+const express=require('express');
+const app=express();
+const port=8000;
+app.listen(port,function(err){
+    if(err){
+        console.log(`server is giving an error:${err}`)
+    }
+    console.log(`server is running on port : ${ port}`)
+})
