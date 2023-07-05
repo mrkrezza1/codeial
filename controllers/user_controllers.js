ea9c420cd92c390c3b391d93c3e365264ef8e8dd
mrkrezza1 <sajit1020@gmail.com>
2023-07-06 03:29:51 +0530

    i am trying to remove the remove and update the image without taking the more space but unable to do that task

--- controllers/user_controllers.js
+++ controllers/user_controllers.js
@@ -1,116 +1,126 @@
-const User=require('../models/users')//here we are importing the data
-
+const User = require('../models/users')//here we are importing the data
+const fs = require('fs')
+const path = require('path')
 // rendering the profile from the user
-module.exports.profile =async function(req, res){
+module.exports.profile = async function (req, res) {
     try {
-        let user=await User.findById(req.params.id)
+        let user = await User.findById(req.params.id)
 
         return res.render('users', {
             title: 'User Profile',
-             profile_user:user
-    })  
-    } catch (error) {
-      console.log("error in the profile",error)  
-    }     
-        
-    
-}
-// updating the users email and name
-module.exports.update=async function(req,res){
-    if(req.user.id==req.params.id){
-    try {
-        let user=await User.findById(req.params.id,req.body)
-        User.uploadedAvatar(req,res,function(err){
-            if(err){
-                console.log("multer error",err)
-            }
-            user.name=req.body.name;
-            user.email=req.body.email;
-            if(req.file){
-                user.avatar=User.avatarPath+"/"+req.file.filename;
-            }
-            user.save();
-            console.log(req.file)
-            return res.redirect('back')
+            profile_user: user
         })
     } catch (error) {
-        req.flash("error",err)
-        // console.log("error in updating")
-        return res.redirect('back');
+        console.log("error in the profile", error)
     }
-        
-    }else{
-        req.flash("error",'unauthorized')
+
+
+}
+// updating the users email and name
+module.exports.update = async function (req, res) {
+    if (req.user.id == req.params.id) {
+        try {
+            let user = await User.findById(req.params.id, req.body)
+            User.uploadedAvatar(req, res, function (err) {
+                if (err) {
+                    console.log("multer error", err)
+                }
+                user.name = req.body.name;
+                user.email = req.body.email;
+
+                if (req.file) {
+                    console.log("crashing in req file")
+                    // this is saving the path path of uploaded file into avatar field in the user
+                    // if (user.avatar) {
+                    //     console.log("crashing in avatar sfile")
+                    //     fs.unlinkSync(path.join(__dirname, '..', user.avatar))
+                    // }
+                    user.avatar = User.avatarPath + "/" + req.file.filename;
+                }
+                user.save();
+                console.log("if return ")
+                return res.redirect('back')
+            })
+        } catch (error) {
+            console.log("catch")
+            req.flash("error", err)
+            // console.log("error in updating")
+            return res.redirect('back');
+        }
+
+    } else {
+        console.log("outer else")
+        req.flash("error", 'unauthorized')
         return res.status(401).send("unauthorized")
     }
 }
 
 // rendering the sign up page
-module.exports.signUp=async function(req,res){
+module.exports.signUp = async function (req, res) {
     try {
-        await res.render('user_sign_up',{
-            title:"user | sign-up"
+        await res.render('user_sign_up', {
+            title: "user | sign-up"
         });
-        
+
     } catch (error) {
-        console.log("error in sign up",error)
+        console.log("error in sign up", error)
     }
 }
 
 // rendering the sign in page 
-module.exports.signIn=async function(req,res){
-try {
-    await res.render('user_sign_in',{
-        title:"user | sign-in"
-    })
-    
-} catch (error) {
-    console.log("error in sign in",error)
-}
+module.exports.signIn = async function (req, res) {
+    try {
+        await res.render('user_sign_in', {
+            title: "user | sign-in"
+        })
+
+    } catch (error) {
+        console.log("error in sign in", error)
+    }
 }
 
 // get the sign up data
 // first check password is not equal
 
 
-module.exports.create =async function(req, res){
-    if (req.body.password !== req.body.confirm_password){
+module.exports.create = async function (req, res) {
+    if (req.body.password !== req.body.confirm_password) {
         return res.redirect('back');
     }
-try{
+    try {
 
-    let user=await User.findOne({email: req.body.email})
-    
-    if (!user){
-        console.log("user doesnt exist so we are creating the user")
-        user=await User.create(req.body);
-        if(!user){
-            console.log("error in creating the user while we sign in")
-            return res.redirect('/users/sign-in');
+        let user = await User.findOne({ email: req.body.email })
+
+        if (!user) {
+            console.log("user doesnt exist so we are creating the user")
+            user = await User.create(req.body);
+            if (!user) {
+                console.log("error in creating the user while we sign in")
+                return res.redirect('/users/sign-in');
+            }
         }
-    }
-            
+
         return res.redirect('/');
 
-}
-catch(err){
-    console.log(err)
-}
+    }
+    catch (err) {
+        console.log(err)
+    }
 }
 
 // here we are loging in existing account
-module.exports.createSession=function(req,res){
-    req.flash('success',"loged in successfully")
+module.exports.createSession = function (req, res) {
+    req.flash('success', "loged in successfully")
     return res.redirect('/');
 }
 
 // creating for the logout router
-module.exports.destroySession=function(req,res){
-    req.logout(req.user,err=>{
-        if(err){
+module.exports.destroySession = function (req, res) {
+    req.logout(req.user, err => {
+        if (err) {
             return;
         }
-        req.flash('success',"loged out successfully")
+        req.flash('success', "loged out successfully")
         return res.redirect('/');
-       });
+    });
 }
