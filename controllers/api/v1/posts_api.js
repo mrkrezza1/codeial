1371d6dcbb435947df3c9c7887911867fbd1b7da
mrkrezza1 <sajit1020@gmail.com>
2023-07-08 14:49:19 +0530

    here we have trying to create the action between the route and controller to delete the post and comment of user in the created api

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,38 @@
-module.exports.index=function(req,res){
-    return res.json(200,{
+const Post=require('../../../models/post')
+const Comment=require('../../../models/comment')
+// const { post } = require('../../../routes/api')
+module.exports.index=async function(req,res){
+        let posts=await Post.find({})
+        .sort('-createdAt')
+        .populate('user')
+        .populate({
+            path:'comments',
+            populate:{
+                path:'user'
+            }
+        })
+
+     return res.json(200,{
         message:"lists of posts",
-        posts:[]
+        posts:posts
     })
 }
+
+
+    module.exports.destroy = async function (req, res) {
+        try {
+            let post = await Post.findById(req.params.id);
+            post.remove();
+            
+            await Comment.deleteMany({ post: req.params.id })
+    
+            
+            return res.json(200,{
+                message:"post and its assosciated comment has been deleted successfully"
+            })
+        } catch (err) {
+            return res.json(500,{
+                message:"error in the internal server"
+            });
+        }
+    }
