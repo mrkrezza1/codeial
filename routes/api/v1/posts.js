609e3d812054376ed4e32fc557452a4127227c6a
mrkrezza1 <sajit1020@gmail.com>
2023-07-12 16:45:28 +0530

    here we have tried to change somthing

--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -1,6 +1,8 @@
 const express=require('express')
+// const passport=require('passport')
 const router=express.Router();
-const postApi=require("../../../controllers/api/v1/posts_api")
+const postApi=require("../../../controllers/api/v1/posts_api");
+const passport = require('passport');
 router.get('/',postApi.index)
-router.delete('/:id',postApi.destroy)
+router.delete('/:id',passport.authenticate('jwt',{session: false}),postApi.destroy)
 module.exports=router;
--- routes/api/v2/posts.js
+++ routes/api/v2/posts.js
@@ -1,5 +1,7 @@
 const express=require('express');
+const passport=require('passport')
 const router=express.Router();
 const postApi=require("../../../controllers/api/v2/posts_api")
 router.get('/',postApi.index)
+router.delete('/:id',passport.authenticate('jwt',{session:false}),postApi.destroy)
 module.exports=router;
